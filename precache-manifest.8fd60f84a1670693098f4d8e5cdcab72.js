79ec09ba66fb934b7449e7a1ff4844fa40b4b55e
Peter <cirediatpl@gmail.com>
2019-07-29 14:07:15 -0400

    Updates

---
+++ precache-manifest.8fd60f84a1670693098f4d8e5cdcab72.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b97ecd9a110cc691f4026fcf10017bc",
+    "url": "/fluz-coding-challenge/index.html"
+  },
+  {
+    "revision": "7eb18a95de522f1bec6e",
+    "url": "/fluz-coding-challenge/static/css/main.1c1adc1e.chunk.css"
+  },
+  {
+    "revision": "015af1e8945680218359",
+    "url": "/fluz-coding-challenge/static/js/2.c830b613.chunk.js"
+  },
+  {
+    "revision": "7eb18a95de522f1bec6e",
+    "url": "/fluz-coding-challenge/static/js/main.30f042e1.chunk.js"
+  },
+  {
+    "revision": "543da77ed7c1134c98b0",
+    "url": "/fluz-coding-challenge/static/js/runtime~main.deab386e.js"
+  },
+  {
+    "revision": "341c2ce9d0f0404676603cb9f2da5cae",
+    "url": "/fluz-coding-challenge/static/media/itunes.341c2ce9.jpg"
+  },
+  {
+    "revision": "f97adb3aecad9365b38035b7511b7743",
+    "url": "/fluz-coding-challenge/static/media/itunes.f97adb3a.svg"
+  },
+  {
+    "revision": "0520f594048ba8c39d5f59c9bb81aba7",
+    "url": "/fluz-coding-challenge/static/media/nike.0520f594.svg"
+  },
+  {
+    "revision": "b965b1e3d15d87d6416a9343a9f56835",
+    "url": "/fluz-coding-challenge/static/media/nike.b965b1e3.jpg"
+  },
+  {
+    "revision": "b870a7ddc4699bf33f855059144d7561",
+    "url": "/fluz-coding-challenge/static/media/panera.b870a7dd.jpg"
+  },
+  {
+    "revision": "ec9208097d7c06bb494390efde1649fc",
+    "url": "/fluz-coding-challenge/static/media/panera.ec920809.svg"
+  },
+  {
+    "revision": "028c9ee11f66e081ec9b7793147fc287",
+    "url": "/fluz-coding-challenge/static/media/starbucks.028c9ee1.jpg"
+  },
+  {
+    "revision": "99286d1ea735c4f3a22d46905342cf8c",
+    "url": "/fluz-coding-challenge/static/media/starbucks.99286d1e.svg"
+  }
+]);
